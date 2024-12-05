e9b7d96fc980ae188ef12a102cbfed239ff97185
Zachary Taylor <z2.taylor@connect.qut.edu.au>
2024-12-05 22:41:35 +1100

    did part on of day 5, part 2 is cooked

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,130 @@
+use std::collections::BTreeMap;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let rules_contents = fs::read_to_string("rules.txt").expect("Error loading file");
+    let pages_contents = fs::read_to_string("pages.txt").expect("Error loading file");
+    println!(
+        "part 1 answer: {}",
+        part_one(rules_contents.as_str(), pages_contents.as_str())
+    );
+    println!(
+        "part 2 answer: {}",
+        part_two(rules_contents.as_str(), pages_contents.as_str())
+    );
+}
+
+fn get_rules(raw_rules: &str) -> BTreeMap<i32, Vec<i32>> {
+    let mut rules_map: BTreeMap<i32, Vec<i32>> = BTreeMap::new();
+    for line in raw_rules.lines() {
+        let pair_vec = line
+            .split('|')
+            .map(|split| split.parse().unwrap())
+            .collect::<Vec<i32>>();
+
+        rules_map.entry(pair_vec[0]).or_default().push(pair_vec[1]);
+    }
+
+    rules_map
+}
+
+fn get_pages(raw_pages: &str) -> Vec<Vec<i32>> {
+    raw_pages
+        .lines()
+        .map(|line| {
+            line.split(',')
+                .map(|split| split.parse().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn part_one(raw_rules: &str, raw_pages: &str) -> i32 {
+    let pages = get_pages(raw_pages);
+    let rules_map = get_rules(raw_rules);
+
+    let mut sum = 0;
+    for page in pages {
+        if let Some(result) = search_one(&page[..], &rules_map) {
+            sum += result;
+        }
+    }
+
+    sum
+}
+
+fn search_one(page: &[i32], rules: &BTreeMap<i32, Vec<i32>>) -> Option<i32> {
+    // for each element
+    for i in 0..page.len() {
+        // check every following element
+        for j in 0..i {
+            if let Some(rule) = rules.get(&(page[i] as i32)) {
+                if rule.contains(&page[j]) {
+                    return None;
+                }
+            }
+        }
+    }
+
+    return Some(page[(page.len() as f32 / 2.0_f32).floor() as usize]);
+}
+
+fn search_two(mut numbers: Vec<i32>, rules: &BTreeMap<i32, Vec<i32>>) -> Option<i32> {
+    None
+}
+
+fn part_two(raw_rules: &str, raw_pages: &str) -> i32 {
+    let pages = get_pages(raw_pages);
+    let rules_map = get_rules(raw_rules);
+
+    let mut sum = 0;
+    for page in pages {
+        if let Some(result) = search_two(page, &rules_map) {
+            sum += result;
+        }
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_RULES: &str = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13";
+    const TEST_PAGES: &str = "75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one(TEST_RULES, TEST_PAGES), 143)
+    }
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(TEST_RULES, TEST_PAGES), 123)
+    }
 }
