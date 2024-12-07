11af4d0108198bad942987742b1b5b881ff4bd98
Zachary Taylor <z2.taylor@connect.qut.edu.au>
2024-12-07 18:32:14 +1100

    disgusting brute force solution

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, ops::RemAssign};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum CellTypes {
     Empty,
     Wall,
@@ -49,7 +49,7 @@ impl Direction {
         }
     }
 
-    fn is_left(&self, other: &Self) -> bool {
+    fn is_left_of(&self, other: &Self) -> bool {
         match other {
             Self::Up => self == &Self::Left,
             Self::Right => self == &Self::Up,
@@ -87,13 +87,10 @@ fn part_one(input: &str) -> i32 {
                 pos = result;
             }
             CellTypes::Wall => direction.turn(),
-            _ => {
-                panic!("shouldn't be here")
-            }
         }
     }
 
-    println!("{}", debug_grid(&occupied_map));
+    // println!("{}", debug_grid(&occupied_map));
 
     occupied_map
         .iter()
@@ -130,7 +127,7 @@ fn debug_grid(grid: &[Vec<bool>]) -> String {
     grid.iter()
         .map(|row| {
             row.iter()
-                .map(|&cell| if cell { '#' } else { '.' })
+                .map(|&cell| if cell { '$' } else { '.' })
                 .collect::<String>()
         })
         .collect::<Vec<String>>()
@@ -138,51 +135,47 @@ fn debug_grid(grid: &[Vec<bool>]) -> String {
 }
 
 fn part_two(input: &str) -> i32 {
-    let (mut pos, mut current_direction, map) = init_state(input);
+    let (initital_pos, initial_direction, map) = init_state(input);
 
-    let mut occupied_map: Vec<Vec<Option<Vec<Direction>>>> =
-        vec![vec![None; map[0].len()]; map.len()];
-    occupied_map[pos.0 as usize][pos.1 as usize] = Some(vec![Direction::Up]);
+    let limit = 4 * map.len() * map[0].len();
     let mut sum = 0;
-    'outer: loop {
-        let result = pos.clone() + current_direction.direction();
-        if result.0 < 0
-            || result.1 < 0
-            || result.0 >= map.len() as i32
-            || result.1 >= map[0].len() as i32
-        {
-            break 'outer;
-        }
 
-        match map[result.0 as usize][result.1 as usize] {
-            CellTypes::Empty => {
-                if let Some(ref mut stored_directions) =
-                    &mut occupied_map[result.0 as usize][result.1 as usize]
-                {
-                    for stored_direction in stored_directions.iter() {
-                        println!(
-                            "I'm facing {current_direction:?}. last time, I was facing {stored_direction:?}"
-                        );
-                        if current_direction.is_left(&stored_direction) {
-                            println!("{result:?}");
-                            sum += 1;
-                            break;
-                        }
-                    }
+    for i in 0..map.len() {
+        for j in 0..map[0].len() {
+            println!("{i}, {j}");
+            let mut direction = initial_direction.clone();
+            let mut pos = initital_pos.clone();
+            let mut num_moves = 0;
+            let mut new_map = map.clone();
+            // Skip this if map is already wall
+            if new_map[i][j] == CellTypes::Wall {
+                continue;
+            }
+            new_map[i][j] = CellTypes::Wall;
 
-                    stored_directions.push(current_direction.clone());
-                } else {
-                    occupied_map[pos.0 as usize][pos.1 as usize] =
-                        Some(vec![current_direction.clone()]);
+            'outer: loop {
+                // loop detected
+                if num_moves >= limit {
+                    sum += 1;
+                    break;
                 }
 
-                pos = result;
-            }
-            CellTypes::Wall => {
-                current_direction.turn();
-            }
-            _ => {
-                panic!("shouldn't be here")
+                let result = pos.clone() + direction.direction();
+                if result.0 < 0
+                    || result.1 < 0
+                    || result.0 >= map.len() as i32
+                    || result.1 >= map[0].len() as i32
+                {
+                    break 'outer;
+                }
+
+                match new_map[result.0 as usize][result.1 as usize] {
+                    CellTypes::Empty => {
+                        num_moves += 1;
+                        pos = result;
+                    }
+                    CellTypes::Wall => direction.turn(),
+                }
             }
         }
     }
@@ -205,20 +198,20 @@ mod tests {
 #.........
 ......#...";
 
-    #[test]
-    fn test_part_one() {
-        assert_eq!(part_one(TEST_INPUT), 41)
-    }
+    // #[test]
+    // fn test_part_one() {
+    //     assert_eq!(part_one(TEST_INPUT), 41)
+    // }
     #[test]
     fn test_part_two() {
         assert_eq!(part_two(TEST_INPUT), 6)
     }
 
-    #[test]
-    fn test_is_left() {
-        let current_dir = Direction::Left;
-        let other_dir = Direction::Up;
+    // #[test]
+    // fn test_is_left() {
+    //     let current_dir = Direction::Left;
+    //     let other_dir = Direction::Up;
 
-        assert!(current_dir.is_left(&other_dir));
-    }
+    //     assert!(current_dir.is_left_of(&other_dir));
+    // }
 }
