f6a5a43400d8c2c657051df60023c9dd8a645eac
Zachary Taylor <z2.taylor@connect.qut.edu.au>
2024-12-08 17:14:08 +1100

    shifted things around a bit

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BTreeMap, HashMap, HashSet},
+    collections::{BTreeMap, HashSet},
     fs,
 };
 
@@ -7,51 +7,6 @@ fn main() {
     let contents = fs::read_to_string("input.txt").expect("Error loading file");
     println!("part 1 answer: {}", part_one(contents.as_str()));
     println!("part 2 answer: {}", part_two(contents.as_str()));
-}
-
-fn part_one(input: &str) -> u64 {
-    let (map, bounds) = parse_input(input);
-
-    let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
-
-    for (key, vec) in map {
-        for (i, (row, col)) in vec.iter().enumerate() {
-            for j in i..vec.len() {
-                if i == j {
-                    continue;
-                }
-                let (other_row, other_col) = vec[j];
-
-                let (diff_row, diff_col) = ((row - other_row).abs(), (col - other_col).abs());
-
-                let node_1 = if *col < other_col {
-                    (row - diff_row, col - diff_col)
-                } else {
-                    (row - diff_row, col + diff_col)
-                };
-
-                let node_2 = if *col < other_col {
-                    (other_row + diff_row, other_col + diff_col)
-                } else {
-                    (other_row + diff_row, other_col - diff_col)
-                };
-
-                if test_node(node_1, bounds) {
-                    antinodes.insert(node_1);
-                }
-
-                if test_node(node_2, bounds) {
-                    antinodes.insert(node_2);
-                }
-            }
-        }
-    }
-
-    antinodes.len() as u64
-}
-
-fn test_node(node: (i32, i32), bounds: (u32, u32)) -> bool {
-    !(node.0 < 0 || node.1 < 0 || node.0 >= bounds.0 as i32 || node.1 >= bounds.1 as i32)
 }
 
 fn parse_input(input: &str) -> (BTreeMap<char, Vec<(i32, i32)>>, (u32, u32)) {
@@ -72,32 +27,54 @@ fn parse_input(input: &str) -> (BTreeMap<char, Vec<(i32, i32)>>, (u32, u32)) {
     (map, bounds)
 }
 
-fn print_grid(rows: i32, cols: i32, filled_cells: &HashSet<(i32, i32)>) {
-    for row in 0..rows {
-        for col in 0..cols {
-            if filled_cells.contains(&(row, col)) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!(); // Move to the next row
-    }
-}
-
-fn part_two(input: &str) -> u64 {
-    let (map, bounds) = parse_input(input);
+fn part_one(input: &str) -> u64 {
+    let (towers, bounds) = parse_input(input);
 
     let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
 
-    for (_key, vec) in map {
+    for vec in towers.values() {
+        for (i, (row, col)) in vec.iter().enumerate() {
+            for j in (i + 1)..vec.len() {
+                let (other_row, other_col) = vec[j];
+
+                let (diff_row, diff_col) = ((row - other_row).abs(), (col - other_col).abs());
+
+                let node_1 = if *col < other_col {
+                    (row - diff_row, col - diff_col)
+                } else {
+                    (row - diff_row, col + diff_col)
+                };
+
+                let node_2 = if *col < other_col {
+                    (other_row + diff_row, other_col + diff_col)
+                } else {
+                    (other_row + diff_row, other_col - diff_col)
+                };
+
+                if is_node_in_bounds(node_1, bounds) {
+                    antinodes.insert(node_1);
+                }
+
+                if is_node_in_bounds(node_2, bounds) {
+                    antinodes.insert(node_2);
+                }
+            }
+        }
+    }
+
+    antinodes.len() as u64
+}
+
+fn part_two(input: &str) -> u64 {
+    let (towers, bounds) = parse_input(input);
+
+    let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
+
+    for vec in towers.values() {
         for (i, (row, col)) in vec.iter().enumerate() {
             antinodes.insert((*row, *col));
 
-            for j in i..vec.len() {
-                if i == j {
-                    continue;
-                }
+            for j in (i + 1)..vec.len() {
                 let (other_row, other_col) = vec[j];
 
                 let (diff_row, diff_col) = ((row - other_row).abs(), (col - other_col).abs());
@@ -107,7 +84,7 @@ fn part_two(input: &str) -> u64 {
                 } else {
                     (row - diff_row, col + diff_col)
                 };
-                while test_node(node_1, bounds) {
+                while is_node_in_bounds(node_1, bounds) {
                     antinodes.insert(node_1);
                     node_1 = if *col < other_col {
                         (node_1.0 - diff_row, node_1.1 - diff_col)
@@ -122,7 +99,7 @@ fn part_two(input: &str) -> u64 {
                     (other_row + diff_row, other_col - diff_col)
                 };
 
-                while test_node(node_2, bounds) {
+                while is_node_in_bounds(node_2, bounds) {
                     antinodes.insert(node_2);
                     node_2 = if *col < other_col {
                         (node_2.0 + diff_row, node_2.1 + diff_col)
@@ -137,6 +114,23 @@ fn part_two(input: &str) -> u64 {
     print_grid(bounds.0 as i32, bounds.1 as i32, &antinodes);
 
     antinodes.len() as u64
+}
+
+fn is_node_in_bounds(node: (i32, i32), bounds: (u32, u32)) -> bool {
+    !(node.0 < 0 || node.1 < 0 || node.0 >= bounds.0 as i32 || node.1 >= bounds.1 as i32)
+}
+
+fn print_grid(rows: i32, cols: i32, filled_cells: &HashSet<(i32, i32)>) {
+    for row in 0..rows {
+        for col in 0..cols {
+            if filled_cells.contains(&(row, col)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!(); // Move to the next row
+    }
 }
 
 #[cfg(test)]
